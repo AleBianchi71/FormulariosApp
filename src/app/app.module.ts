15f144236e431e7e17d3c2288edd4d64fc1fa1ed
AleBianchi71 <bianchialejandro998@gmail.com>
2023-03-29 08:38:45 -0300

    Creación del Formulario

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { BasicosComponent } from './template/basicos/basicos.component';
+import { DinamicosComponent } from './template/dinamicos/dinamicos.component';
+import { SwitchesComponent } from './template/switches/switches.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BasicosComponent,
+    DinamicosComponent,
+    SwitchesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
